739c4ef92905dd3c4a4b8c5b57736bb6100438a8
vlimki <vlimki@proton.me>
2025-05-04 23:35:38 +0300

    loosen type bounds on Vector

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use bytemuck::Zeroable;
 use cust::memory::*;
 use cust::module::Module;
 use cust::launch;
@@ -33,13 +34,16 @@ impl Default for CudaCtx {
     }
 }
 
-struct Vector {
-    _device_ptr: Option<DeviceBuffer<f32>>,
-    _inner: Vec<f32>
+pub struct Vector<T>
+where 
+    T: DeviceCopy
+{
+    _device_ptr: Option<DeviceBuffer<T>>,
+    _inner: Vec<T>
 }
 
-impl From<Vec<f32>> for Vector {
-    fn from(v: Vec<f32>) -> Self {
+impl<T: DeviceCopy> From<Vec<T>> for Vector<T> {
+    fn from(v: Vec<T>) -> Self {
         Self {
             _inner: v,
             _device_ptr: None,
@@ -47,20 +51,22 @@ impl From<Vec<f32>> for Vector {
     }
 }
 
-impl PartialEq for Vector {
+impl<T: DeviceCopy + PartialEq> PartialEq for Vector<T> {
     fn eq(&self, other: &Self) -> bool {
         self._inner == other._inner
     }
 }
 
-impl Drop for Vector {
+impl<T: DeviceCopy> Drop for Vector<T> {
     fn drop(&mut self) {
         let _ = self._device_ptr.take();
     }
 }
 
-impl Add for Vector {
-    type Output = Vector;
+impl<T> Add for Vector<T>
+where T: Default + DeviceCopy + Zeroable
+{
+    type Output = Vector<T>;
 
     fn add(mut self, mut other: Self) -> Self {
         assert_eq!(self._inner.len(), other._inner.len());
@@ -69,7 +75,7 @@ impl Add for Vector {
         self._device_ptr = Some(DeviceBuffer::from_slice(&self._inner).unwrap());
         other._device_ptr = Some(DeviceBuffer::from_slice(&other._inner).unwrap());
 
-        let c_out: DeviceBuffer<f32> = DeviceBuffer::zeroed(self._inner.len()).unwrap();
+        let c_out: DeviceBuffer<T> = DeviceBuffer::zeroed(self._inner.len()).unwrap();
 
         let ctx = CUDA_CTX.lock().unwrap();
         let (module, stream) = (&ctx.module, &ctx.stream);
@@ -84,7 +90,7 @@ impl Add for Vector {
             result.unwrap()
         }
 
-        let mut host_out = vec![0.0f32; self._inner.len()];
+        let mut host_out = vec![T::default(); self._inner.len()];
         c_out.copy_to(&mut host_out[..]).unwrap();
 
         ctx.stream.synchronize().unwrap();
@@ -102,11 +108,11 @@ mod tests {
 
     #[test]
     fn vec_add() {
-        let v1 = Vector::from(vec![1.0,2.0,3.0]);
-        let v2 = Vector::from(vec![2.0,4.0,6.0]);
+        let v1: Vector<f32> = Vector::from(vec![1.0, 2.0, 3.0]);
+        let v2 = Vector::from(vec![2.0, 4.0, 6.0]);
 
         let v3 = v1 + v2;
-        assert_eq!(v3._inner, vec![3.0,6.0,9.0]); 
+        assert_eq!(v3._inner, vec![3.0, 6.0, 9.0]); 
     }
 
 }
